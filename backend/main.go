f0a6bef14f5567733f1ee0d08654bcf746f578c5
ahmadhabibi14 <ahmadhabibi7159@gmail.com>
2022-12-25 15:54:53 +0800

    Downgrade

--- backend/main.go
+++ backend/main.go
@@ -1,28 +1,49 @@
 package main
 
 import (
-	"backend/controllers"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
-func main() {
-	r := setupRouter()
-	_ = r.Run(":8080")
+type Film struct {
+	ID    int    `json:"id" gorm:"primaryKey"`
+	Title string `json:"title"`
+	Year  int    `json:"release_year"`
+	Genre string `json:"genre"`
 }
 
-func setupRouter() *gin.Engine {
-	r := gin.Default()
-	r.GET("ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, "pong")
-	})
-	bookRepo := controllers.New()
-	r.POST("/books", bookRepo.CreateBook)
-	r.GET("/books", bookRepo.GetBooks)
-	r.GET("/books/:id", bookRepo.GetBook)
-	r.PUT("/books/:id", bookRepo.UpdateBook)
-	r.DELETE("/books/:id", bookRepo.DeleteBook)
+func Connect() (*gorm.DB, error) {
+	dsn := "root@tcp(127.0.0.1:3306)/movie_web"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		fmt.Println("Error DB :::::::: !!!")
+	}
 
-	return r
+	if err = db.AutoMigrate(&Film{}); err != nil {
+		fmt.Println("AutoMigrate ERROR :::::::: !!!")
+	}
+	return db, err
+}
+
+func getAllFilms(c *gin.Context) {
+	var Films []Film
+	db, err := Connect()
+	if err != nil {
+		fmt.Println("ERROR GET ::::::::::::: !!!")
+	}
+
+	if err := db.Find(&Films).Error; err != nil {
+		fmt.Println("ERROR QUERY :::::::::: !!!")
+	}
+	c.JSON(http.StatusOK, Films)
+}
+
+func main() {
+	route := gin.Default()
+	route.GET("/films", getAllFilms)
+	route.Run("localhost:8080") // listen and serve on 0.0.0.0:8080
 }
